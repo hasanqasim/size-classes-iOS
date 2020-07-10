2141b7c2233f641b42c567f5f0c2c6d76d355bcc
Hasan Qasim <hasanqasim92@gmail.com>
2020-07-10 12:19:01 +1000

    Initial Commit

---
+++ size-classes-iPhone-iPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  size-classes-iPhone-iPad
+//
+//  Created by Hasan Qasim on 10/7/20.
+//  Copyright © 2020 Hasan Qasim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
