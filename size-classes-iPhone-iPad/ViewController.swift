fa9d24e0d7916bb365da5f1c99b3bfcb8a506b17
Hasan Qasim <hasanqasim92@gmail.com>
2020-07-10 13:31:05 +1000

    add iPhone UI using auto layout constraints

--- size-classes-iPhone-iPad/ViewController.swift
+++ size-classes-iPhone-iPad/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoIV: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        logoIV.layer.cornerRadius = 5
+        
     }
 
 
